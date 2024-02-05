6235f5510ef42b2effd031b70a7ad301dcd472e7
Igor Brusko <tailsmiles2015b@gmail.com>
2024-02-05 19:04:25 +0700

    Проверка на наличие папки ресурсов

--- Editor/SettingsProviderCreator.cs
+++ Editor/SettingsProviderCreator.cs
@@ -8,7 +8,11 @@ namespace BlackTailsUnityTools.Runtime
         [MenuItem("BlackTailsTools/Create settings provider in resources")]
         static void CreateEnemies()
         {
+            var resourcesPath = "Assets/Resources";
             var settingsProvider = ScriptableObject.CreateInstance<SettingsProvider>();
+            if (!AssetDatabase.IsValidFolder(resourcesPath))
+                AssetDatabase.CreateFolder("Assets", "Resources");
+            
             AssetDatabase.CreateAsset(settingsProvider, $"Assets/Resources/SettingsProvider.asset");
         }
     }
